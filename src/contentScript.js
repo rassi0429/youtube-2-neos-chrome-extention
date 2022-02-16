2e92210d4b32f34c6c555afa048fd8aa1c2b96c5
rassi0429 <taitokokoa+rassi@gmail.com>
2022-02-17 07:38:57 +0900

    feat: ok

--- src/contentScript.js
+++ src/contentScript.js
@@ -14,30 +14,81 @@
 // Log `title` of current active web page
 const pageTitle = document.head.getElementsByTagName('title')[0].innerHTML;
 console.log(
-  `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
+  `Page title is : '${pageTitle}'`
 );
+let socket = null
+
+connectWS("ciel")
+
+function connectWS(uniqueId) {
+  socket = new WebSocket("wss://wsecho.kokoa.dev/kokoa0429/yt2neos/" + uniqueId)
+  socket.addEventListener('message', function (event) {
+    if (event.data.startsWith("toS")) console.log(event.data)
+    const commands = event.data.split(":")
+
+    switch (commands[1]) {
+      case "pause":
+        document.getElementsByTagName("video")[0].click()
+        break
+      case "next":
+        document.getElementsByClassName("ytp-next-button")[0].click()
+        break
+      case "play":
+        const videoId = commands[2]// youtube_parser(commands[2])
+        if(videoId) {
+          location.href = `https://youtube.com/watch?v=${videoId}`
+        }
+        break
+    }
+  });
+}
+
+
+
+function getInfomation() {
+  try {
+    const videoTag = document.getElementsByTagName("video")[0]
+    const duration = videoTag.duration
+    const currentTime = videoTag.currentTime
+    const title = document.getElementsByClassName("title style-scope ytd-video-primary-info-renderer")[0].innerText
+    const channelName = document.getElementById("channel-name").innerText
+    const id = youtube_parser(document.URL)
+    if (socket) {
+      socket.send([currentTime, duration, title, channelName, id].join("$#"))
+    }
+  } catch { }
+}
+
+setInterval(() => getInfomation(), 1000)
+
+function youtube_parser(url) {
+  var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  var match = url.match(regExp);
+  return (match && match[7].length == 11) ? match[7] : false;
+}
 
 // Communicate with background file by sending a message
-chrome.runtime.sendMessage(
-  {
-    type: 'GREETINGS',
-    payload: {
-      message: 'Hello, my name is Con. I am from ContentScript.',
-    },
-  },
-  response => {
-    console.log(response.message);
-  }
-);
+// chrome.runtime.sendMessage(
+//   {
+//     type: 'GREETINGS',
+//     payload: {
+//       message: 'Hello, my name is Con. I am from ContentScript.',
+//     },
+//   },
+//   response => {
+//     console.log(response.message);
+//   }
+// );
 
-// Listen for message
+// // Listen for message
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'COUNT') {
-    console.log(`Current count is ${request.payload.count}`);
+  if (request.type === 'ws') {
+    console.log(`ws connect req: ${request.payload.connect}`);
+    connectWS(request.payload.uniqueId)
   }
-
-  // Send an empty response
-  // See https://github.com/mozilla/webextension-polyfill/issues/130#issuecomment-531531890
-  sendResponse({});
-  return true;
-});
+})
+//   // Send an empty response
+//   // See https://github.com/mozilla/webextension-polyfill/issues/130#issuecomment-531531890
+//   sendResponse({});
+//   return true;
+// });
--- src/popup.js
+++ src/popup.js
@@ -2,7 +2,7 @@
 
 import './popup.css';
 
-(function() {
+(function () {
   // We will make use of Storage API to get and store `count` value
   // More information on Storage API can we found at
   // https://developer.chrome.com/extensions/storage
@@ -10,16 +10,16 @@ import './popup.css';
   // To get storage access, we have to mention it in `permissions` property of manifest.json file
   // More information on Permissions can we found at
   // https://developer.chrome.com/extensions/declare_permissions
-  const counterStorage = {
-    get: cb => {
-      chrome.storage.sync.get(['count'], result => {
-        cb(result.count);
+  const storage = {
+    getUniqueId: cb => {
+      chrome.storage.sync.get(['uniqueId'], result => {
+        cb(result.uniqueId);
       });
     },
-    set: (value, cb) => {
+    setUniqueId: (value, cb) => {
       chrome.storage.sync.set(
         {
-          count: value,
+          uniqueId: value,
         },
         () => {
           cb();
@@ -28,85 +28,116 @@ import './popup.css';
     },
   };
 
-  function setupCounter(initialValue = 0) {
-    document.getElementById('counter').innerHTML = initialValue;
 
-    document.getElementById('incrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'INCREMENT',
-      });
-    });
+  function setup() {
+    document.getElementById('connectButton').addEventListener('click', () => {
 
-    document.getElementById('decrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'DECREMENT',
-      });
-    });
-  }
-
-  function updateCounter({ type }) {
-    counterStorage.get(count => {
-      let newCount;
-
-      if (type === 'INCREMENT') {
-        newCount = count + 1;
-      } else if (type === 'DECREMENT') {
-        newCount = count - 1;
-      } else {
-        newCount = count;
-      }
-
-      counterStorage.set(newCount, () => {
-        document.getElementById('counter').innerHTML = newCount;
-
-        // Communicate with content script of
-        // active tab by sending a message
+      storage.getUniqueId((v) => {
         chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
           const tab = tabs[0];
-
           chrome.tabs.sendMessage(
             tab.id,
             {
-              type: 'COUNT',
+              type: 'ws',
               payload: {
-                count: newCount,
+                connect: true,
+                uniqueId: v,
               },
             },
-            response => {
-              console.log('Current count value passed to contentScript file');
-            }
+            response => { }
           );
         });
       });
     });
-  }
-
-  function restoreCounter() {
-    // Restore count value
-    counterStorage.get(count => {
-      if (typeof count === 'undefined') {
-        // Set counter value as 0
-        counterStorage.set(0, () => {
-          setupCounter(0);
-        });
-      } else {
-        setupCounter(count);
-      }
+    storage.getUniqueId((v) => {
+      console.log(v)
+      document.getElementById("uniqueId").value = v || "UNIQUE_ID"
+    })
+    document.getElementById('uniqueId').addEventListener('change', (evt) => {
+      console.log(evt.target.value)
+      storage.setUniqueId(evt.target.value, () => { })
     });
   }
 
-  document.addEventListener('DOMContentLoaded', restoreCounter);
+  // function setupCounter(initialValue = 0) {
+  //   document.getElementById('counter').innerHTML = initialValue;
+
+  //   document.getElementById('incrementBtn').addEventListener('click', () => {
+  //     updateCounter({
+  //       type: 'INCREMENT',
+  //     });
+  //   });
+
+  //   document.getElementById('decrementBtn').addEventListener('click', () => {
+  //     updateCounter({
+  //       type: 'DECREMENT',
+  //     });
+  //   });
+  // }
+
+  // function updateCounter({ type }) {
+  //   counterStorage.get(count => {
+  //     let newCount;
+
+  //     if (type === 'INCREMENT') {
+  //       newCount = count + 1;
+  //     } else if (type === 'DECREMENT') {
+  //       newCount = count - 1;
+  //     } else {
+  //       newCount = count;
+  //     }
+
+  //     counterStorage.set(newCount, () => {
+  //       document.getElementById('counter').innerHTML = newCount;
+
+  //       // Communicate with content script of
+  //       // active tab by sending a message
+  //       chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+  //         const tab = tabs[0];
+
+  //         chrome.tabs.sendMessage(
+  //           tab.id,
+  //           {
+  //             type: 'COUNT',
+  //             payload: {
+  //               count: newCount,
+  //             },
+  //           },
+  //           response => {
+  //             console.log('Current count value passed to contentScript file');
+  //           }
+  //         );
+  //       });
+  //     });
+  //   });
+  // }
+
+  function restoreCounter() {
+    // Restore count value
+    // counterStorage.get(count => {
+    //   if (typeof count === 'undefined') {
+    //     // Set counter value as 0
+    //     counterStorage.set(0, () => {
+    //       setupCounter(0);
+    //     });
+    //   } else {
+    //     setupCounter(count);
+    //   }
+    // });
+  }
+
+  document.addEventListener('DOMContentLoaded', setup());
 
   // Communicate with background file by sending a message
-  chrome.runtime.sendMessage(
-    {
-      type: 'GREETINGS',
-      payload: {
-        message: 'Hello, my name is Pop. I am from Popup.',
-      },
-    },
-    response => {
-      console.log(response.message);
-    }
-  );
+  // chrome.runtime.sendMessage(
+  //   {
+  //     type: 'GREETINGS',
+  //     payload: {
+  //       message: 'Hello, my name is Pop. I am from Popup.',
+  //     },
+  //   },
+  //   response => {
+  //     console.log(response.message);
+  //   }
+  // );
 })();
